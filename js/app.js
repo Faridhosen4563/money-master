972a38d54bd0ff4e9f30fdf1c33518a76dd0597e
Faridhosen4563 <faridhosen790@gmail.com>
2022-08-19 23:55:19 +0600

    can not use faka or nagative

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ function getValueByInput(id) {
     const inputValueString = inputFieldElement.value;
     const inputValue = parseFloat(inputValueString);
     return inputValue;
+
 }
 
 function setTextValue(id, newValue) {
     const textElemnet = document.getElementById(id);
     textElemnet.innerText = newValue;
+
 }
 
 document.getElementById("calculate-btn").addEventListener('click', function () {
@@ -15,27 +17,77 @@ document.getElementById("calculate-btn").addEventListener('click', function () {
     const rent = getValueByInput("rent-value");
     const clothes = getValueByInput("clothes-value");
 
-    const totalExpenses = food + rent + clothes;
-
-    setTextValue("total-expenses", totalExpenses);
+    if (isNaN(food) == false && isNaN(rent) == false && isNaN(clothes) == false) {
+        if (food < 0 || rent < 0 || clothes < 0) {
+            alert("Please provid positive value, you cant cost nagitive.")
+            return;
+        }
+    }
+    else {
+        alert("enter value.")
+        return;
+    }
 
     const income = getValueByInput("total-income");
+    if (isNaN(income) == false) {
+        if (income < 0) {
+            alert("You cant income nagitive.")
+            return;
+        }
+    }
+    else {
+        alert("Enter Income Value");
+        return;
+    }
+    const totalExpenses = food + rent + clothes;
+    if (totalExpenses > income) {
+        alert(`you cant spend ${totalExpenses}$, Because your current income ${income}$`);
+        return;
+    }
+    setTextValue("total-expenses", totalExpenses);
 
     const newBalance = income - totalExpenses;
+
     setTextValue("balance", newBalance);
 })
 
 document.getElementById("save-btn").addEventListener('click', function () {
     const income = getValueByInput("total-income");
     const savingParcent = getValueByInput("saving");
+    if (isNaN(income) == false) {
+        if (income < 0) {
+            alert("dont")
+            return;
+        }
+    }
+    else {
+        alert("Enter Income Value");
+        return;
+    }
+    if (isNaN(savingParcent) == false) {
+        if (savingParcent < 0) {
+            alert("Saving cannot nagative.")
+            return;
+        }
+    }
+    else {
+        alert("Enter saving Value");
+        return;
+    }
 
     const saving = income * (savingParcent / 100);
 
-    setTextValue("saving-amount", saving);
+
 
     const totalBalanceElemnt = document.getElementById("balance");
     const totalBalanceString = totalBalanceElemnt.innerText;
     const totalBalanceNumber = parseFloat(totalBalanceString);
+
+    if (saving > totalBalanceNumber) {
+        alert(`You cant save ${saving}$, Because your balance now ${totalBalanceNumber}$.`)
+        return;
+    }
+    setTextValue("saving-amount", saving);
 
     const remainingAmount = totalBalanceNumber - saving;
 
